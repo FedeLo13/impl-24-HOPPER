37de7c5ac262a39bc60f567b5cb023dda8cf134e
FedeLo13 <fede.lopezpe@alum.uca.es>
2024-05-06 18:10:47 +0200

    Cambios a la clase robot

--- temas/abstraccion/ruby-01/src/robot.rb
+++ temas/abstraccion/ruby-01/src/robot.rb
@@ -1,28 +1,43 @@
+require_relative 'hablador'
+
 class Robot
+    include Hablador
+
+    # Definición de los métodos de acceso a las variables de instancia (con uso de symbols)
     attr_accessor :nombre
     attr_reader :material
+    attr_writer :num_laseres
 
-    def initialize(nombre, material)
+    # Constructor de la clase
+    def initialize(nombre, material, num_laseres)
         @nombre = nombre
         @material = material
-        @acciones = []
-    end
-        
-    def mover(direccion)
-        @acciones << "Moviendo hacia #{direccion}"
+        @num_laseres = num_laseres
     end
 
-    def girar(direccion)
-        @acciones << "Girando hacia #{direccion}"
+    # Definición del método hablar del mixin Hablador, accesible desde cualquier parte del código
+    def hablar
+        "HOLA. SOY. UN. ROBOT. MI. NOMBRE. ES. #{@nombre} Y. ESTOY. HECHO. DE. #{@material}."
     end
 
-    def ejecutar_acciones
-        @acciones.each do |accion|
-            puts accion
+    # Método de instancia, accesible desde cualquier parte del código
+    def disparar_laser
+        if @num_laseres > 0
+            puts "PEW! "
+            @num_laseres -= 1
+        else
+            puts "NO. TENGO. MAS. LASERES."
+            recargar_laseres
         end
     end
 
-    def objetivo
-        raise NotImplementedError, "Debe implementar el método objetivo en la subclase"
+    private
+
+    # Método privado, solo accesible desde la clase
+    def recargar_laseres
+        puts "RECARGANDO. LASERES."
+        laseres = rand(1..10)
+        @num_laseres += laseres
+        puts "RECARGA. COMPLETA. AHORA. TENGO. #{@num_laseres} LASERES."
     end
 end
