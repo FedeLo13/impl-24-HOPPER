a6167798d94bbbbae075aa579bfdb393bcbeb3fc
FedeLo13 <fede.lopezpe@alum.uca.es>
2024-05-10 10:40:21 +0200

    Creada la clase Robot Inteligente

--- temas/herencia/dart/lib/robot.dart
+++ temas/herencia/dart/lib/robot.dart
@@ -1,24 +1,24 @@
 abstract class Robot {
   String nombre;
   String material;
-  final List<String> _acciones = [];
+  final List<String> acciones = [];
 
   Robot(this.nombre, this.material);
 
   //Acciones de cualquier robot
 
   void mover(String direccion) {
-    _acciones.add("$nombre moviéndose hacia $direccion");
+    acciones.add("$nombre moviéndose hacia $direccion");
   }
 
   void girar(String direccion) {
-    _acciones.add("$nombre girando hacia $direccion");
+    acciones.add("$nombre girando hacia $direccion");
   }
 
   // Ejecutar las acciones del robot
 
   void ejecutarAcciones() {
-    for (var accion in _acciones) {
+    for (var accion in acciones) {
       print(accion);
     }
   }
