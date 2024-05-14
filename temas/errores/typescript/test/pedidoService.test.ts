37903d1d2dc2e5c411614aad0a2de685bc5218c1
Mauro <mauro.clavoconesa@alum.uca.es>
2024-05-14 21:41:06 +0200

    Actualizados los test con los nuevos nombres

--- temas/errores/typescript/test/pedidoService.test.ts
+++ temas/errores/typescript/test/pedidoService.test.ts
@@ -1,65 +1,65 @@
 import { PedidoService, PedidoError } from './../src/pedidoService';
-import { Restaurante, Plato, PlatoError } from './../src/restaurante';
+import { Almacen, Articulo, ArticuloError } from '../src/almacen';
 
 describe('PedidoService', () => {
     let pedidoService: PedidoService;
-    let restaurante: Restaurante;
+    let almacen: Almacen;
 
     beforeEach(() => {
-        restaurante = new Restaurante();
-        pedidoService = new PedidoService(restaurante);
+        almacen = new Almacen();
+        pedidoService = new PedidoService(almacen);
     });
 
     test('realizarPedido lanza un error si el pedido está vacío', async () => {
         // Pedido vacío
-        const pedidoVacio: Plato[] = [];
+        const pedidoVacio: Articulo[] = [];
 
         // Debe lanzar un error de PedidoError.PedidoVacio
         await expect(pedidoService.realizarPedido(pedidoVacio)).rejects.toThrow(PedidoError.PedidoVacio());
     });
 
-    test('realizarPedido actualiza el stock de los platos y devuelve undefined si el pedido se realiza con éxito', async () => {
-        // Configuración de los platos en el restaurante
-        const plato1: Plato = { nombre: 'Plato1', cantidad: 10 };
-        const plato2: Plato = { nombre: 'Plato2', cantidad: 5 };
-        restaurante.insertarPlato(plato1);
-        restaurante.insertarPlato(plato2);
+    test('realizarPedido actualiza el stock de los articulos y devuelve undefined si el pedido se realiza con éxito', async () => {
+        // Configuración de los articulos en el restaurante
+        const articulo1: Articulo = { nombre: 'articulo1', cantidad: 10 };
+        const articulo2: Articulo = { nombre: 'articulo2', cantidad: 5 };
+        almacen.insertarArticulo(articulo1);
+        almacen.insertarArticulo(articulo2);
 
         // Pedido exitoso
-        const pedidoExitoso: Plato[] = [
-            { nombre: 'Plato1', cantidad: 2 },
-            { nombre: 'Plato2', cantidad: 3 }
+        const pedidoExitoso: Articulo[] = [
+            { nombre: 'articulo1', cantidad: 2 },
+            { nombre: 'articulo2', cantidad: 3 }
         ];
 
         await expect(pedidoService.realizarPedido(pedidoExitoso)).resolves.toBeUndefined();
 
         // Comprobación de que el stock se ha actualizado correctamente
-        expect(restaurante.obtenerPlatosDisponibles()).toEqual([
-            { nombre: 'Plato1', cantidad: 8 },
-            { nombre: 'Plato2', cantidad: 2 }
+        expect(almacen.obtenerArticulosDisponibles()).toEqual([
+            { nombre: 'articulo1', cantidad: 8 },
+            { nombre: 'articulo2', cantidad: 2 }
         ]);
     });
 
-    test('realizarPedido lanza un error si el plato no existe en el restaurante', async () => {
-        // Pedido con plato inexistente
-        const pedidoPlatoInexistente: Plato[] = [
-            { nombre: 'PlatoInexistente', cantidad: 2 }
+    test('realizarPedido lanza un error si el articulo no existe en el restaurante', async () => {
+        // Pedido con articulo inexistente
+        const pedidoarticuloInexistente: Articulo[] = [
+            { nombre: 'articuloInexistente', cantidad: 2 }
         ];
 
-        // Debe lanzar un error de PlatoError.PlatoNoEncontrado
-        await expect(pedidoService.realizarPedido(pedidoPlatoInexistente)).rejects.toThrow(PlatoError.PlatoNoEncontrado('PlatoInexistente'));
+        // Debe lanzar un error de articuloError.articuloNoEncontrado
+        await expect(pedidoService.realizarPedido(pedidoarticuloInexistente)).rejects.toThrow(ArticuloError.ArticuloNoEncontrado('articuloInexistente'));
     });
 
     test('realizarPedido lanza un error si no hay suficiente stock para el pedido', async () => {
-        // Configuración de los platos en el restaurante
-        const plato: Plato = { nombre: 'Plato3', cantidad: 2 };
-        restaurante.insertarPlato(plato);
+        // Configuración de los articulos en el restaurante
+        const articulo: Articulo = { nombre: 'articulo3', cantidad: 2 };
+        almacen.insertarArticulo(articulo);
 
         // Pedido con cantidad mayor que el stock disponible
-        const pedidoCantidadInsuficiente: Plato[] = [
-            { nombre: 'Plato3', cantidad: 3 }
+        const pedidoCantidadInsuficiente: Articulo[] = [
+            { nombre: 'articulo3', cantidad: 3 }
         ];
 
-        await expect(pedidoService.realizarPedido(pedidoCantidadInsuficiente)).rejects.toThrow(PlatoError.PlatoStockInsuficiente('Plato3'));
+        await expect(pedidoService.realizarPedido(pedidoCantidadInsuficiente)).rejects.toThrow(ArticuloError.ArticuloStockInsuficiente('articulo3'));
     });
 });
